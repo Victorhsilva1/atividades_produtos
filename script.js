e4946aa4c36a6536158e2f61d283e83d53087275
Victorhsilva1 <victor.hsilva2703@gmail.com>
2025-08-26 08:54:41 -0300

    documentando javaScript e reduzindo html

--- script.js
+++ script.js
@@ -3,15 +3,24 @@
 
 import produtos from "./produtos_atualizados.json" with {type: "json"}
  
+//Selecionando o elemento items será o container para todos os cards.
 const container = document.querySelector('.items')
  
 const criarCard = (produto) => {
+
+    //Criando as constantes como cada elemento do html
     const card = document.createElement('div')
     card.classList.add('card')
  
+
+    //Ou seja criando a constante imagem e definindo que 
     const imagem = document.createElement('img')
+    //Haverá um cardImg que será a class desse elemento
     imagem.classList.add('cardImg')
+    //Pegando a imagem do json
     imagem.src = `./img/${produto.imagem}`
+    // E pegando o nome do produto no json também e assim sucessivamente para cada 
+    // um dos elementos presentes no json, e transferindo ele como html
     imagem.alt = produto.nome
  
     const titulo = document.createElement('h4')
@@ -33,15 +42,18 @@ const criarCard = (produto) => {
     const preco = document.createElement('p')
     preco.classList.add('card-price')
 
-
+    //Adicionando os elementos em cada div
     card.append(imagem, titulo, descricao, ratingContainer, preco)
  
+    //Retornando a função card
     return card
 }
  
 const carregarProdutos = () => {
 
+    //Utilizando o map para chamar a função de CallBack que "varre os arrays"
     const cards = produtos.map(criarCard)
+    //forEach ou seja, até que exista produtos irá repitir o mesmo processo
     cards.forEach(card => container.appendChild(card))
 }
  
